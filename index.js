3ff894d5f656f9c4831199bfa34930e0b51636fc
csernakakos <csernakakos@gmail.com>
2022-02-14 20:21:12 +0100

    Parsed URL manually

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json({true: "hi there!"});
+    res.send(`
+        <div>
+            <form action="/" method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordconfirmation" placeholder="password confirmation" />
+
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
 });
+
+app.post("/", (req, res) => {
+    req.on("data", data => {
+        const parsed = data.toString("utf8").split("&");
+        const formData = {};
+        for (let pair of parsed) {
+            const [key, value] = pair.split("=");
+            formData[key] = value;
+        }
+        console.log(formData);
+    });
+    res.send("Signed up!")
+})
 
 app.listen(3020, () => {console.log("running!")});
