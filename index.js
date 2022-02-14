02c074d24d885f65f32cca272c525d574327702b
csernakakos <csernakakos@gmail.com>
2022-02-14 20:37:20 +0100

    Improved our own middleware

--- index.js
+++ index.js
@@ -18,17 +18,35 @@ app.get("/", (req, res) => {
     `);
 });
 
-app.post("/", (req, res) => {
-    req.on("data", data => {
-        const parsed = data.toString("utf8").split("&");
-        const formData = {};
-        for (let pair of parsed) {
-            const [key, value] = pair.split("=");
-            formData[key] = value;
-        }
-        console.log(formData);
-    });
+// My custom URL parser middleware
+function bodyParser(req, res, next){
+    if (req.method === "POST") {
+        req.on("data", data => {
+            const parsed = data.toString("utf8").split("&");
+            const formData = {};
+            for (let pair of parsed) {
+                const [key, value] = pair.split("=");
+                formData[key] = value;
+            }
+            req.body = formData;
+            next();
+        });
+
+    } else {
+        next();
+    }
+};
+
+// Let's run yet another middleware:
+function helloFromMiddleware(req, res, next){
+    console.log("Hello from middleware!");
+    next();
+};
+
+// Using data from middleware
+app.post("/", bodyParser, helloFromMiddleware, (req, res) => {
+    console.log(req.body);
     res.send("Signed up!")
-})
+});
 
 app.listen(3020, () => {console.log("running!")});
